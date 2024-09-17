fcbbe053f448a4022ee1d66e9ad170c094814e39
Jen <jen@example.com>
2024-09-17 13:56:39 +0100

    actioned feedback by adding back arrow and removed loading state

--- src/components/SingleArticleCard.jsx
+++ src/components/SingleArticleCard.jsx
@@ -2,31 +2,22 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import ListComments from "./ListComments";
+import { Link } from "react-router-dom";
 
 export default function SingleArticleCard() {
   const [articleById, setArticleById] = useState([]);
-
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
   const { article_id } = useParams();
 
   useEffect(() => {
-    setIsLoading(true);
-    setIsError(false);
     axios
       .get("https://nc-news-53nl.onrender.com/api/articles/" + article_id)
       .then((response) => {
         return response.data;
       })
       .then((data) => {
-        setIsLoading(false);
-        setIsError(false);
-        // console.log(data[0]);
         setArticleById(data[0]);
       })
       .catch((error) => {
-        setIsLoading(false);
-        setIsError(true);
       });
   }, []);
 
@@ -35,6 +26,9 @@ export default function SingleArticleCard() {
 
   return (
     <>
+    <div id="back-arrow">
+    <Link to={'/'}><i class="fa-sharp fa-solid fa-arrow-left"></i></Link>
+    </div>
       <div className="article-container">
         <div className="article">
           <h2>{articleById.title}</h2>
